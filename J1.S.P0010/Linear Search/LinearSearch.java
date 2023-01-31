61717983cb2968b27ca6ba216f604cd2ae728b3a
chunChu <86117554+ChungDoan@users.noreply.github.com>
2023-01-31 11:36:25 +0700

    README.md

---
+++ J1.S.P0010/Linear Search/LinearSearch.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg0010;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lap
+ */
+public class LinearSearch {
+    
+    //check input int
+    public int getInt(String message, String firstError, String secondError, String thirdError, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println(message);
+                String input = scanner.nextLine();
+                int number = Integer.parseInt(input);
+
+                //check range
+                if (number > max) {
+                    System.out.println(firstError);
+                } else if (number < min) {
+                    System.out.println(secondError);
+                } else {
+                    return number;
+                }
+            } catch (Exception e) {
+                //xay ra khi ng dung nhap khong phai so nguyen
+                System.out.println(thirdError);
+            }
+        }
+    }
+    
+    
+    void randomElement(int[] array) {
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            //sinh ra cac so ngau nhien va gasn vao cac phan tu mang
+            array[i] = random.nextInt(array.length - 1) + 1;
+
+        }
+    }
+
+    void search(int[] array, int searchNumber) {
+        for (int i = 0; i < array.length; i++) {
+            if(searchNumber == array[i]) {
+                System.out.println("Found " + searchNumber + " at index " + i );
+                //stop 
+                return;
+            }else {
+                
+            }
+            
+        }System.out.println("Not found.");
+    }
+    
+    
+    
+    
+}
---
+++ J1.S.P0010/Linear Search/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg0010;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Lap
+ */
+public class Main {
+
+   
+
+    public static void main(String[] args) {
+        LinearSearch linearSearch = new LinearSearch();
+
+        //input number of array ( number phai la so nguyen duong)
+        int number = linearSearch.getInt("Enter number of array: ", "wrong1(do chua bic db ycau nhap tu bnh)",
+                "Number must > 0", "Input phai la so", 0, Integer.MAX_VALUE);
+        //create array
+        int[] array = new int[number];
+        
+        //input search number
+        int searchNumber = linearSearch.getInt("Enter number that u want to search: ", "wrong1(do chua bic db ycau nhap tu bnh)",
+                "Number must > 0", "Input phai la so", Integer.MIN_VALUE, Integer.MAX_VALUE);
+        
+        //create random number for elements
+        linearSearch.randomElement(array);
+        
+        //show array
+        System.out.println("The array: " + Arrays.toString(array));
+        
+        //search numer
+        linearSearch.search(array, searchNumber);
+
+
+    }
+}
