530de5b0646dd5e274015c687dcad095523510e6
chunChu <86117554+ChungDoan@users.noreply.github.com>
2023-02-14 00:51:37 +0700

    view

---
+++ J1.S.P0071/Task management/Ver 1/Main.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import controller.ManageTask;
+
+/**
+ *
+ * @author chungdoan
+ */
+public class Main {
+    public static void main(String[] args) {
+        ManageTask manage = new ManageTask();
+        Utility utility = new Utility();
+        while (true) {
+            displayMenu();
+            int option = utility.getInt("Option: ", "Wrong", "Wrong", "Wrong", 1, 4);
+
+            switch (option) {
+                case 1:
+                    //input task
+                    manage.inputTask();
+                    break;
+                case 2:
+                    //delete task
+                    manage.deleteTask();
+                    break;
+                case 3:
+                    //display task
+                    manage.displayTask();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+
+            }
+
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("========= Task program =========\n"
+                + "1.	Add Task\n"
+                + "2.	Delete task\n"
+                + "3.	Display Task\n"
+                + "4.	exit");
+    }
+}
