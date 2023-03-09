53b5ba3ddea4519470643569a706ecff1b7412e9
chunChu <86117554+ChungDoan@users.noreply.github.com>
2023-03-10 00:00:39 +0700

    70 locs

--- J1.S.P0065/Manage Grade/0065/src/controller/Function.java
+++ J1.S.P0065/Manage Grade/0065/src/controller/Function.java
@@ -35,6 +35,22 @@ public class Function {
         listStudent.add(student);
     }
     
+    public HashMap<String, Double> getPercentTypeStudent() {
+        HashMap<String, Double> hashMap = new HashMap<>();
+        hashMap.put("A", 0.0);
+        hashMap.put("B", 0.0);
+        hashMap.put("C", 0.0);
+        hashMap.put("D", 0.0);
+
+        //loop from first to last element of listStudent
+        for (Student student : listStudent) {
+            double value = hashMap.get(student.getType()).doubleValue() + 1;
+            hashMap.replace(student.getType(), value);
+        }
+        return hashMap;
+    }
+    
+    
     //check yes or no 
     static boolean checkYesNo() {
         String result = getString("Do you want to enter more student information?(Y/N):",
