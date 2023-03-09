021ddc74ee7d2fd3e2c8049a46928afec4a4e69c
chunChu <86117554+ChungDoan@users.noreply.github.com>
2023-03-09 08:26:43 +0700

    50 locs

--- J1.S.P0008/Count word/0008/src/pkg0008/Main.java
+++ J1.S.P0008/Count word/0008/src/pkg0008/Main.java
@@ -16,37 +16,57 @@ import javafx.scene.input.KeyCode;
  * @author chungdoan
  */
 public class Main {
-    public void count(String str, String type){
+    public void count(String str, String type) {
+        // Xóa tất cả các ký tự đặc biệt từ chuỗi str
         str = str.replaceAll("\\W", " ");
+        // Xóa tất cả các chữ số từ chuỗi str
         str = str.replaceAll("[0-9]", " ");
+        // Xóa tất cả các khoảng trắng thừa từ chuỗi str
         str = str.replaceAll("  +", " ");
+        // Khởi tạo một Map để lưu trữ số lần xuất hiện của các từ/ký tự
         Map<String, Integer> counter = new HashMap<>();
-        StringTokenizer st = new StringTokenizer(str);  
-        String[] w = new String[st.countTokens()] ;
-        int i=0;
-        if(type.equals("Word")){         
-            while(st.hasMoreTokens()){
-                w[i]=st.nextToken();
+        // Tạo một StringTokenizer từ chuỗi str
+        StringTokenizer st = new StringTokenizer(str);
+        // Khởi tạo một mảng w để lưu trữ các từ hoặc ký tự tùy vào giá trị của biến type
+        String[] w = new String[st.countTokens()];
+        int i = 0;
+        // Nếu type là "Word", thêm các từ vào mảng w
+        
+        /**
+         * 
+         */
+        if (type.equals("Word")) {
+            while (st.hasMoreTokens()) {
+                //Sử dụng vòng lặp while để lấy ra từng token 
+                //tức là từ được tách ra từ chuỗi đầu vào bởi phương thức StringTokenizer) và lưu vào mảng w.
+                w[i] = st.nextToken();
+                // Tăng biến i lên mỗi khi một từ được lưu vào mảng w.
                 i++;
-            }          
-        }
-        if(type.equals("Char")){
-            str= str.replaceAll(" +", "");
-            w=str.split("");
-        }
-        for (String w1 : w) {
-            if(!counter.containsKey(w1)){
-                counter.put(w1, 1);
-            }else{
-                counter.put(w1, (int)counter.get(w1)+1);
             }
         }
+        // Nếu type là "Char", thêm các ký tự vào mảng w
+        if (type.equals("Char")) {
+            str = str.replaceAll(" +", "");
+            w = str.split("");
+        }
+        // Tăng giá trị tương ứng trong Map cho mỗi từ/ký tự
+        for (String w1 : w) {
+            if (!counter.containsKey(w1)) {
+                counter.put(w1, 1);
+            } else {
+                counter.put(w1, (int) counter.get(w1) + 1);
+            }
+        }
+        // In ra Map để hiển thị số lần xuất hiện của các từ/ký tự
         System.out.println(counter);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine() .trim();
+        // Nhập chuỗi từ người dùng
+        String str = sc.nextLine().trim();
         Main main = new Main();
+        // Đếm số lần xuất hiện của từng từ/ký tự trong chuỗi và hiển thị kết quả
         main.count(str, "Word");
         main.count(str, "Char");
     }
